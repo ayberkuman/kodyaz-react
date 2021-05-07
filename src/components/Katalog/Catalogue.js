a358d188eac731b5c6f6935a43327354b9ea9969
ayberkuman <ayberkuman1@gmail.com>
2021-05-07 23:37:33 +0300

    cat layout

--- src/components/Katalog/Catalogue.js
+++ src/components/Katalog/Catalogue.js
@@ -2,119 +2,137 @@ import "./style.scss";
 import htmlSvg from "./htmlLogo.svg";
 import { Link } from "react-router-dom";
 import Button from "../Landing/Button";
+import Button2 from "../Landing/Button";
 
 const Catalogue = () => {
   return (
     <div className="catWrapper">
       <div className="card">
-        <h1>HTML</h1>
-
-        <img src={htmlSvg} alt="logo" />
-        <div className="info">
-          <h3>HTML</h3>
-          <p>
-            Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-            işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir.
-            HTML için aslında bir web sitesinin iskeleti denilebilir.
-          </p>
-          <Button name={"Hemen Öğren"} />
+        <div className="imgBx">
+          <img src={htmlSvg} alt="" />
+        </div>
+        <div className="contentBx">
+          <div className="content">
+            <h3>HTML</h3>
+            <p>
+              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
+              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
+              ve web sitelerinin iskeleti denilebilir.
+            </p>
+            <Button2 name={"Hemen Öğren"} />
+          </div>
         </div>
       </div>
       <div className="card">
-        <h1>HTML</h1>
-
-        <img src={htmlSvg} alt="logo" />
-        <div className="info">
-          <h3>HTML</h3>
-          <p>
-            Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-            işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir.
-            HTML için aslında bir web sitesinin iskeleti denilebilir.
-          </p>
-          <Button name={"Hemen Öğren"} />
+        <div className="imgBx">
+          <img src={htmlSvg} alt="" />
+        </div>
+        <div className="contentBx">
+          <div className="content">
+            <h3>HTML</h3>
+            <p>
+              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
+              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
+              ve web sitelerinin iskeleti denilebilir.
+            </p>
+            <Button2 name={"Hemen Öğren"} />
+          </div>
         </div>
       </div>
       <div className="card">
-        <h1>HTML</h1>
-
-        <img src={htmlSvg} alt="logo" />
-        <div className="info">
-          <h3>HTML</h3>
-          <p>
-            Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-            işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir.
-            HTML için aslında bir web sitesinin iskeleti denilebilir.
-          </p>
-          <Button name={"Hemen Öğren"} />
+        <div className="imgBx">
+          <img src={htmlSvg} alt="" />
+        </div>
+        <div className="contentBx">
+          <div className="content">
+            <h3>HTML</h3>
+            <p>
+              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
+              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
+              ve web sitelerinin iskeleti denilebilir.
+            </p>
+            <Button2 name={"Hemen Öğren"} />
+          </div>
         </div>
       </div>
       <div className="card">
-        <h1>HTML</h1>
-
-        <img src={htmlSvg} alt="logo" />
-        <div className="info">
-          <h3>HTML</h3>
-          <p>
-            Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-            işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir.
-            HTML için aslında bir web sitesinin iskeleti denilebilir.
-          </p>
-          <Button name={"Hemen Öğren"} />
+        <div className="imgBx">
+          <img src={htmlSvg} alt="" />
+        </div>
+        <div className="contentBx">
+          <div className="content">
+            <h3>HTML</h3>
+            <p>
+              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
+              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
+              ve web sitelerinin iskeleti denilebilir.
+            </p>
+            <Button2 name={"Hemen Öğren"} />
+          </div>
         </div>
       </div>
       <div className="card">
-        <h1>HTML</h1>
-
-        <img src={htmlSvg} alt="logo" />
-        <div className="info">
-          <h3>HTML</h3>
-          <p>
-            Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-            işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir.
-            HTML için aslında bir web sitesinin iskeleti denilebilir.
-          </p>
-          <Button name={"Hemen Öğren"} />
+        <div className="imgBx">
+          <img src={htmlSvg} alt="" />
+        </div>
+        <div className="contentBx">
+          <div className="content">
+            <h3>HTML</h3>
+            <p>
+              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
+              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
+              ve web sitelerinin iskeleti denilebilir.
+            </p>
+            <Button2 name={"Hemen Öğren"} />
+          </div>
         </div>
       </div>
       <div className="card">
-        <h1>HTML</h1>
-
-        <img src={htmlSvg} alt="logo" />
-        <div className="info">
-          <h3>HTML</h3>
-          <p>
-            Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-            işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir.
-            HTML için aslında bir web sitesinin iskeleti denilebilir.
-          </p>
-          <Button name={"Hemen Öğren"} />
+        <div className="imgBx">
+          <img src={htmlSvg} alt="" />
+        </div>
+        <div className="contentBx">
+          <div className="content">
+            <h3>HTML</h3>
+            <p>
+              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
+              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
+              ve web sitelerinin iskeleti denilebilir.
+            </p>
+            <Button2 className="btn" name={"Hemen Öğren"} />
+          </div>
         </div>
       </div>
       <div className="card">
-        <h1>HTML</h1>
-
-        <img src={htmlSvg} alt="logo" />
-        <div className="info">
-          <h3>HTML</h3>
-          <p>
-            Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-            işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir.
-            HTML için aslında bir web sitesinin iskeleti denilebilir.
-          </p>
-          <Button name={"Hemen Öğren"} />
+        <div className="imgBx">
+          <img src={htmlSvg} alt="" />
+        </div>
+        <div className="contentBx">
+          <div className="content">
+            <h3>HTML</h3>
+            <p>
+              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
+              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
+              ve web sitelerinin iskeleti denilebilir.
+            </p>
+            <Button2 className="btn" name={"Hemen Öğren"} />
+          </div>
         </div>
       </div>
       <div className="card">
-        <h1>HTML</h1>
-        <img src={htmlSvg} alt="logo" />
-        <div className="info">
-          <h3>HTML</h3>
-          <p>
-            Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-            işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir.
-            HTML için aslında bir web sitesinin iskeleti denilebilir.
-          </p>
-          <Button name={"Hemen Öğren"} />
+        <div className="imgBx">
+          <img src={htmlSvg} alt="" />
+        </div>
+        <div className="contentBx">
+          <div className="content">
+            <h3>HTML</h3>
+            <p>
+              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
+              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
+              ve web sitelerinin iskeleti denilebilir.
+            </p>
+            <Button2 className="btn" name={"Hemen Öğren"} />
+          </div>
         </div>
       </div>
     </div>

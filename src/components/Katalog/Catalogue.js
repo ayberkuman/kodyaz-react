166aee419ab5c8e94dc69d64d9440d901ee460a9
ayberkuman <ayberkuman1@gmail.com>
2021-05-08 22:50:18 +0300

    gasp anims

--- src/components/Katalog/Catalogue.js
+++ src/components/Katalog/Catalogue.js
@@ -3,138 +3,55 @@ import htmlSvg from "./htmlLogo.svg";
 import { Link } from "react-router-dom";
 import Button from "../Landing/Button";
 import Button2 from "../Landing/Button";
+import gsap, { Power2, TweenMax } from "gsap";
+import { useRef, useEffect } from "react";
+
+const MapThis = ({ animationEnter, mouseLeave }) => {
+  return (
+    <div
+      onMouseEnter={animationEnter}
+      onMouseLeave={mouseLeave}
+      className="card"
+    >
+      <div className="imgBx">
+        <img src={htmlSvg} alt="" />
+      </div>
+      <div className="contentBx">
+        <div className="content">
+          <h3>HTML</h3>
+          <p>
+            Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
+            işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir ve
+            web sitelerinin iskeleti denilebilir.
+          </p>
+          <Button2 name={"Hemen Öğren"} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const Catalogue = () => {
+  const animationMaker = () => {
+    let contentAnim = document.querySelectorAll(".contentBx");
+    gsap.to(contentAnim, {
+      display: "flex",
+      ease: Power2.easeOut,
+      duration: 0.1,
+      y: 30,
+    });
+  };
+  const mouseLeave = () => {
+    let contentAnim = document.querySelectorAll(".contentBx");
+    gsap.to(contentAnim, {
+      display: "none",
+      duration: 0.1,
+    });
+  };
+
   return (
     <div className="catWrapper">
-      <div className="card">
-        <div className="imgBx">
-          <img src={htmlSvg} alt="" />
-        </div>
-        <div className="contentBx">
-          <div className="content">
-            <h3>HTML</h3>
-            <p>
-              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
-              ve web sitelerinin iskeleti denilebilir.
-            </p>
-            <Button2 name={"Hemen Öğren"} />
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="imgBx">
-          <img src={htmlSvg} alt="" />
-        </div>
-        <div className="contentBx">
-          <div className="content">
-            <h3>HTML</h3>
-            <p>
-              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
-              ve web sitelerinin iskeleti denilebilir.
-            </p>
-            <Button2 name={"Hemen Öğren"} />
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="imgBx">
-          <img src={htmlSvg} alt="" />
-        </div>
-        <div className="contentBx">
-          <div className="content">
-            <h3>HTML</h3>
-            <p>
-              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
-              ve web sitelerinin iskeleti denilebilir.
-            </p>
-            <Button2 name={"Hemen Öğren"} />
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="imgBx">
-          <img src={htmlSvg} alt="" />
-        </div>
-        <div className="contentBx">
-          <div className="content">
-            <h3>HTML</h3>
-            <p>
-              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
-              ve web sitelerinin iskeleti denilebilir.
-            </p>
-            <Button2 name={"Hemen Öğren"} />
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="imgBx">
-          <img src={htmlSvg} alt="" />
-        </div>
-        <div className="contentBx">
-          <div className="content">
-            <h3>HTML</h3>
-            <p>
-              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
-              ve web sitelerinin iskeleti denilebilir.
-            </p>
-            <Button2 name={"Hemen Öğren"} />
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="imgBx">
-          <img src={htmlSvg} alt="" />
-        </div>
-        <div className="contentBx">
-          <div className="content">
-            <h3>HTML</h3>
-            <p>
-              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
-              ve web sitelerinin iskeleti denilebilir.
-            </p>
-            <Button2 className="btn" name={"Hemen Öğren"} />
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="imgBx">
-          <img src={htmlSvg} alt="" />
-        </div>
-        <div className="contentBx">
-          <div className="content">
-            <h3>HTML</h3>
-            <p>
-              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
-              ve web sitelerinin iskeleti denilebilir.
-            </p>
-            <Button2 className="btn" name={"Hemen Öğren"} />
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="imgBx">
-          <img src={htmlSvg} alt="" />
-        </div>
-        <div className="contentBx">
-          <div className="content">
-            <h3>HTML</h3>
-            <p>
-              Web sayfalarını oluşturma aşamasında kullanılan standart bir metin
-              işaret dili olan HTML,“Hyper Text Markup Language” olarak bilinir
-              ve web sitelerinin iskeleti denilebilir.
-            </p>
-            <Button2 className="btn" name={"Hemen Öğren"} />
-          </div>
-        </div>
-      </div>
+      <MapThis animationEnter={animationMaker} mouseLeave={mouseLeave} />
     </div>
   );
 };
